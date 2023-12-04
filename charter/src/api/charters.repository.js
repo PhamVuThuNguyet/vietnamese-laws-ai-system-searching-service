7dec9b7ad8cedf5d40e118940a1d6f5f9efa81f2
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2023-12-04 13:42:46 +0700

    Add license to each source file

--- charter/src/api/charters.repository.js
+++ charter/src/api/charters.repository.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) VKU.NewEnergy.
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
 const { dataSource } = require('../config/database/data-source');
 const CharterEntity = require('./entities/charter.entity');
 const { Like, OR } = require('typeorm');
